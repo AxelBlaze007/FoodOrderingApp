24e4c19697b2ecc449deef388e785f747fed6df2
AxelBlaze007 <roypannalal007@gmail.com>
2025-04-13 00:58:09 +0530

    React setup Done

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// *
+const Components = () => <h1>demo</h1>;
+const title = (
+        <h1>Helo</h1>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(title);
