e3dec02669171d4c7f1f25508e7854b5f6ae3d2f
AxelBlaze007 <roypannalal007@gmail.com>
2025-04-17 20:52:45 +0530

    new useState keyword used for filtering out

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,15 +13,21 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.3440997&lng=85.309562&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
+    const swiggyURL =
+      "https://www.swiggy.com/dapi/restaurants/list/v5?" +
+      "lat=23.3440997&lng=85.309562&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"; 
+    // const data = await fetch(
+    //   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.3440997&lng=85.309562&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    const data = await fetch("https://api.codetabs.com/v1/proxy?quest=" + encodeURIComponent(swiggyURL));
+      
+    // );
     const json = await data.json();
 
     setListOfRestaurants(
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
-    console.log(json);
+    console.log(encodeURIComponent(swiggyURL));
+    console.log(decodeURI(encodeURI(swiggyURL)));
     // optional chaining
     setFetchRestaurants(
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
@@ -47,11 +53,10 @@ const Body = () => {
             onClick={() => {
               //filter restro card and Update the UI
               //searchText
-              
-              
+
               const filteredRestaurants = fetchRestaurants.filter((res) =>
                 res.info.name.toLowerCase()?.includes(searchText.toLowerCase())
-              )
+              );
 
               setListOfRestaurants(filteredRestaurants);
             }}
